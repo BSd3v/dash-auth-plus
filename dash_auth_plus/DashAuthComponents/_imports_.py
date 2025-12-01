334eb63acb3f1314c37cd6384743a0870db248e7
BSd3v <82055130+BSd3v@users.noreply.github.com>
2025-12-01 05:01:04 -0500

    Delete dash_auth_plus/DashAuthComponents/_imports_.py

--- dash_auth_plus/DashAuthComponents/_imports_.py
+++
@@ -1,7 +0,0 @@
-# AUTO GENERATED FILE - DO NOT EDIT
-
-from .ClerkProvider import ClerkProvider
-from .UserProfile import UserProfile
-from .UserProfilePage import UserProfilePage
-
-__all__ = ["ClerkProvider", "UserProfile", "UserProfilePage"]
