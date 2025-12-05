b1ce90b2d1b0a5801c27bfb002c3eef31d7f71bb
BSd3v <82055130+BSd3v@users.noreply.github.com>
2025-12-05 14:25:36 -0500

    adjustments for imports for packages

--- dash_auth_plus/__init__.py
+++ dash_auth_plus/__init__.py
@@ -14,12 +14,12 @@ try:
     from .oidc_auth import OIDCAuth, get_oauth
     from .clerk_auth import ClerkAuth
     from . import DashAuthComponents
+
+    _css_dist = DashAuthComponents._css_dist
+    _js_dist = DashAuthComponents._js_dist
 except ModuleNotFoundError:
     pass
 from ._version import __version__, __plotly_dash_auth_version__
-
-_css_dist = DashAuthComponents._css_dist
-_js_dist = DashAuthComponents._js_dist
 
 __all__ = [
     "add_public_routes",
